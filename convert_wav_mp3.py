a813e2fd4abd100a0a2a8a8b9ee809843b39aa57
Rastorguev763 <alex.rastorguev@bk.ru>
2023-05-27 13:16:37 +0300

    zz

--- convert_wav_mp3.py
+++ convert_wav_mp3.py
@@ -2,14 +2,20 @@ from pydub import AudioSegment
 import logging
 
 # TODO: подключаем logging к pydub
-# logger = logging.getLogger(__name__)
-# logger.setLevel(logging.DEBUG)
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# handler = logging.StreamHandler()
-# handler.setLevel(logging.DEBUG)
-# handler.setFormatter(formatter)
-# logger.addHandler(handler)
+# Настройка логирования
+# logging.basicConfig(level=logging.DEBUG, filename='app.log',
+#                     format='%(asctime)s %(levelname)s: %(message)s')
+logging.basicConfig(level=logging.DEBUG)
 
+# Создание объекта Logger
+logger = logging.getLogger('pydub')
+
+# Создание обработчика журнала
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+# Добавление обработчика к объекту Logger
+logger.addHandler(handler)
 
 # Указываем путь до исполняющего файла FFMPEG.EXE <'C:\ffmpeg\bin\ffmpeg.exe'>'
 AudioSegment.converter = r'C:\ffmpeg\bin\ffmpeg.exe'

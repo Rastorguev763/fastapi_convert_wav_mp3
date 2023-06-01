f69273fd7990524b5592d676fc6ec713df91bf90
Rastorguev763 <117032024+Rastorguev763@users.noreply.github.com>
2023-06-01 15:54:38 +0300

    Add files via upload

--- convert_wav_mp3.py
+++ convert_wav_mp3.py
@@ -1,10 +1,7 @@
 from pydub import AudioSegment
 import logging, time
 
-# TODO: подключаем logging к pydub
 # Настройка логирования
-# logging.basicConfig(level=logging.DEBUG, filename='app.log',
-#                     format='%(asctime)s %(levelname)s: %(message)s')
 logging.basicConfig(level=logging.DEBUG)
 
 # Создание объекта Logger
@@ -20,13 +17,9 @@ logger.addHandler(handler)
 # Указываем путь до исполняющего файла FFMPEG.EXE <'C:\ffmpeg\bin\ffmpeg.exe'>'
 AudioSegment.converter = r'C:\ffmpeg\bin\ffmpeg.exe'
 
-# file_name = 'sample_1'
-# file_path = f'.\songs\{file_name}.wav'
-
-def convert_wav_to_mp3(data):
-    audio_wav_path = data['wav_path']
-    user_id_create = data['user_id']
-    file_name = audio_wav_path.split('\\')[-1].split('.')[0]
+def convert_wav_to_mp3(audio_file, user_id_create, ):
+    file_name = audio_file.filename.split('.')[0]
+    audio_wav_path = audio_file.file
     current_timestamp = int(time.time())
     song = AudioSegment.from_wav(audio_wav_path)
     file_convert_path = f".\songs\convert\{user_id_create}_{file_name}_convert_{current_timestamp}.mp3"
--- main_web.py
+++ main_web.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body, File, UploadFile, Form
+from fastapi import FastAPI, Body, File, UploadFile, Form, Request
 from fastapi.responses import FileResponse
 from database_module import*
 from convert_wav_mp3 import*
@@ -6,41 +6,42 @@ import shutil
 
 app = FastAPI()
 
-# TODO: POST 
 @app.post("/userinfo")
-def new_user(data = Body()):
+async def new_user(data = Body()):
     user = check_user(data)
     if user:
        user_UUID = user.user_UUID
        user_id = user.user_id
     return {"message": f"АЙДИ ПОЛЬЗОВАТЕЛЯ - {user_id}, УИД ПОЛЬЗОВАТЕЛЯ - {user_UUID}"}
 
-# # TODO: Добавить ссылку для скачивания файла
-# @app.post("/convert")
-# def convert_audio(data = Body()):
-#     # print(audio_wav_path)
-#     file_convert_path = convert_wav_to_mp3(data)
-#     write_audio_info(data, file_convert_path)
-#     return {"message": f"ССЫЛКА ДЛЯ СКАЧИВАНИЯ {file_convert_path}"}
-
-@app.post("/")
-# async def convert_audio(user_id: str, user_UUID: str, audio_file: UploadFile = Body(...)):
-async def convert_audio(user_id: str = Form(...), user_UUID: str = Form(...), audio_file: UploadFile = File(...)):
+@app.post("/convert")
+async def convert_audio(user_id: str = Form(...), audio_file: UploadFile = File(...)):
     # Доступ к сохраненному файлу: audio_file
-    # Доступ к другим данным из формы: user_id, user_UUID
-    print('TRATATATATAT')
-    
-    # Определите путь для сохранения файла
-    save_path = f"songs/convert/{audio_file.filename}"
-    
-    # Сохранение файла на сервере
-    with open(save_path, "wb") as f:
-        shutil.copyfileobj(audio_file.file, f)
+    # Доступ к другим данным из формы: user_id
 
-    return {"message": "Файл аудио успешно загружен и обработан."}
+    try:
+         # Определите путь для сохранения файла
+        filename = f'{user_id}_{audio_file.filename.split(".")[0]}_{int(time.time())}.{audio_file.filename.split(".")[-1]}'
+        save_path = f"songs/{filename}"
+
+        file_convert_path = convert_wav_to_mp3(audio_file, user_id)
+
+            # Сохранение файла на сервере
+        with open(save_path, "wb") as f:
+            shutil.copyfileobj(audio_file.file, f)
+    except:
+        return {"message": f"Произошла ошибка, попробуйте снова."} 
+    
+    return {"file_url": f"/record?file_path={file_convert_path}"}
+# hhtp://host:port/record?id=id_audio&user=user_id
+
+@app.get("/record")
+async def download_file(file_path: str):
+    print("EBALALALLA")
+    return FileResponse(file_path)
 
 @app.get("/")
-def root():
+async def root():
     return FileResponse("public/main.html")
 
 # для запуска сервера используйте команду
